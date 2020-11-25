b4782b9656139326139811428df5f2365a85c492
ATOMICMAN007 <71210312+ATOMICMAN007@users.noreply.github.com>
2020-11-25 11:24:35 +0530

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -22,8 +22,6 @@ class Fun(commands.Cog):
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print('Extension loaded: Fun')
-		sheep_channel = self.bot.get_channel(761864233893494796)
-		await sheep_channel.send('Extension loaded: Fun')
 
 	
 	@commands.command(name="fact", aliases=["ft"])
