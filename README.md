1de1a86a293317d0e22f6590ddc2d48a937e6120
ATOMICMAN007 <71210312+ATOMICMAN007@users.noreply.github.com>
2020-11-25 11:17:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# KyotSheep
+This is my biggest bot so far and has most amount of features.
+
+I'm really new to coding so you might wanna puke after looking at the code.
+Also feel free to steal it and make it your own! (Why am I even saying this. Those who wanna copy will copy anyways. LOL)
+
+# Installation
+As far as documentation goes, I'm lazy enough for now not to make any.
+### Dependecies
+Install:
+1. `discord.py` (rewrite)
+
+2. Make a `.env` file and add `TOKEN="<YOU BOT TOKEN HERE>"
+
+Optional:
+3. `discord-pretty-help`
+
+Makes the default help command look really cool.
+
+Note: If you want the default help command or using your own custom one, then remove-
+
+a. `help_command=PrettyHelp()` from `Bot()` in Line 32 of `main.py`. 
+
+b. `from pretty_help import PrettyHelp` from `main.py`.
---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+from pretty_help import PrettyHelp
+
+
+os.getenv("TOKEN")
+BOT_TOKEN = os.environ["TOKEN"]
+
+def prefix_var(bot, message):
+	prefixes = ['M.']
+
+	if not message.guild:
+		return "M."
+
+	return commands.when_mentioned_or(*prefixes)(bot, message)
+
+bot = Bot(command_prefix=prefix_var, case_insensitive=True, help_command=PrettyHelp(), intents=discord.Intents.all())
+
+
+@bot.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+
+@bot.event
+async def on_guild_join(guild):
+	await print(f"Joined new guild: {guild.name}\nGuild id: {guild.id}")
+
+	
+@bot.event
+async def on_guild_remove(guild):
+	await print(f"Removed from a guild: {guild.name}\nGuild id: {guild.id}")
+
+@bot.command()
+async def ping(ctx):
+	await ctx.send(f'Pong! {round (bot.latency * 1000)}ms')
+
+
+@bot.event
+async def on_ready():
+	print("Bot's Online")
+
+# COGS REGION START
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py'):
+		bot.load_extension(f'cogs.{filename[:-3]}')
+# COGS REGION END
+
+
+# GENERAL ERROR HANDLER EVENT WHICH SENDS A MESSAGE IN CHANNEL.
+@bot.event
+async def on_command_error(ctx, error):
+	if isinstance(error, commands.CommandNotFound):
+		await ctx.send("An error has occured. Please check if you've spelled the command correctly or if it exists at all. `Command Not Found`")
+
+"""
+@bot.event
+async def on_command_error_miss(ctx, error):
+	if isinstance(error, commands.MissingRequiredArguments):
+		await ctx.send("An error has occured. Please check if you've spelled the command correctly or if it exists at all. `Missing Required Arguments`")
+
+"""
+
+
+bot.run(BOT_TOKEN, bot=True, reconnect=True)
